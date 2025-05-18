c5f95a0eb09e4f1ac5291880914baee7caaba399
sanjula pilapitiya <sanjulapilapitiya@hotmail.com>
2025-05-19 00:34:09 +0100

    added change to check if correct branch is updating

--- seeMySpotify/assets/components/login.vue
+++ seeMySpotify/assets/components/login.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>Hello from Vue!</h1>
     <p>This is a basic Vue component running inside Symfony.</p>
+    <p>another test to see if git has the correct branch</p>
   </div>
 </template>
 
